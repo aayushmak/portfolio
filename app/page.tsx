b8f327a65b60d5397d4baeb544cb12b479eb20b5
aayushmak <aayushmakafle@gmail.com>
2025-07-28 12:30:25 +0545

    major changes

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center" >
+      <div>
+        <h1>Hiii</h1>
+      </div>
+    </main>
+  );
+}
